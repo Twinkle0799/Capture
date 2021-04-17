8f0f170ca4ea188fc9c981892b1e4e8f2e8eb81b
Twinkle <gtwinkle132@gmail.com>
2021-04-17 11:34:00 +0530

    Created About Section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+//import pages
+import AboutUs from "./pages/aboutUs";
 
 function App()
 {
   return(
     <div className="App">
-      <h1>Film</h1>
+      <AboutUs/>
     </div>
   );
 }
---
+++ src/pages/aboutUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+//page components
+import AboutSection from '../components/About';
+
+const AboutUs = () => {
+    return(
+        <AboutSection/>
+    )
+}
+
+export default AboutUs;
