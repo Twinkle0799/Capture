8246123c99a6cff50efe08e3847cbcee14ba1667
Twinkle <gtwinkle132@gmail.com>
2021-04-17 16:09:31 +0530

    created faqsection

---
+++ src/components/faq.jsx
@@ -0,0 +1,39 @@
+import React from'react';
+
+const FAqSection = () =>{
+    return(
+        <div className="faq">
+            <h2>Any Questions? <span>Faq</span></h2>
+            <div className="question">
+                <h4>How do I start?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum, ex.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Daily Scheduled</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum, ex.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>Payment Methods?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum, ex.</p>
+                </div>
+            </div>
+            <div className="question">
+                <h4>What products do you offer?</h4>
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum, ex.</p>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+export default FAqSection;
--- src/pages/aboutUs.jsx
+++ src/pages/aboutUs.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 //page components
 import AboutSection from '../components/About';
+import ServicesSection from '../components/services';
+import FAqSection from '../components/faq';
 
 const AboutUs = () => {
     return(
+        <>
         <AboutSection/>
+        <ServicesSection/>
+        <FAqSection/>
+        </>
     )
 }
 
