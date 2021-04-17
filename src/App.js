806752b499b4d735f748563bdfd7c6869c479522
Twinkle <gtwinkle132@gmail.com>
2021-04-17 16:45:17 +0530

    styled componenrs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 //import pages
-import AboutUs from "./pages/aboutUs";
+import AboutUs from "./pages/aboutUs"; 
+//import Globalstyle
+import Globalstyle from './components/globalstyle';
 
 function App()
 {
   return(
     <div className="App">
+      <Globalstyle/>
       <AboutUs/>
     </div>
   );
